7601d4e8a87259f0213d790a5350c99ab81a683f
nelzon <niels.demeyer99@gmail.com>
2024-02-17 05:45:21 +0100

    requests are not blocked

--- scrapy/immoweb/immoweb/spiders/most_expensive.py
+++ scrapy/immoweb/immoweb/spiders/most_expensive.py
@@ -1,10 +1,18 @@
+from typing import Iterable
 import scrapy
 
 
 class MostExpensiveSpider(scrapy.Spider):
     name = "most_expensive"
     allowed_domains = ["www.immoweb.be"]
-    start_urls = ["https://www.immoweb.be/en/"]
-
+    start_urls = ["https://www.immoweb.be/en/search/house/for-sale?countries=BE&page=1&orderBy=most_expensive"]
+    
+    def start_requests(self):
+        for url in self.start_urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+    
     def parse(self, response):
-        pass
+        title = response.css('title::text').get()
+        yield {
+            'title': title,
+        }
