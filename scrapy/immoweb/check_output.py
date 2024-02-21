c4c1a1aabc059a69a1720de2d9af0d000713039f
niels demeyer <niels.demeyer99@gmail.com>
2024-02-21 14:42:20 +0100

    cleanup

--- scrapy/immoweb/check_output.py
+++ scrapy/immoweb/check_output.py
@@ -7,27 +7,27 @@ def read_json_file(filename):
     return data
 
 
-def check_duplicates(data):
+def remove_duplicates(data):
     seen = set()
-    duplicates = []
+    unique_items = []
     for item in data:
         href = item["href"]
-        if href in seen:
-            duplicates.append(item)
-        else:
+        if href not in seen:
+            unique_items.append(item)
             seen.add(href)
-    return duplicates
+    return unique_items
 
 
 # Read data from file
 data = read_json_file("./immoweb/output.json")
 
-# Check for duplicates
-duplicates = check_duplicates(data)
 
-# Print the number of duplicates
-print(len(duplicates))
+# Remove duplicates from data
+unique_data = remove_duplicates(data)
 
-# Print the duplicates
-for duplicate in duplicates:
-    print(duplicate)
+# Print the number of unique items
+print(f"Number of unique items: {len(unique_data)}")
+print(f"Number of duplicate items: {len(data) - len(unique_data)}")
+# Save unique items to a JSON file
+with open("unique_data.json", "w") as f:
+    json.dump(unique_data, f)
