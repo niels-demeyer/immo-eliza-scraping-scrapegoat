98250bc11e68e271ec496688477b5bca93ba2353
niels demeyer <niels.demeyer99@gmail.com>
2024-02-23 12:17:25 +0100

    updated results playwright

--- playwright/FileUtils_class.py
+++ playwright/FileUtils_class.py
@@ -5,13 +5,46 @@ import csv
 
 
 class FileUtils:
+
+    @staticmethod
+    def flatten_dict(d, parent_key="", sep="_"):
+        items = []
+        for k, v in d.items():
+            new_key = f"{parent_key}{sep}{k}" if parent_key else k
+            if isinstance(v, dict):
+                items.extend(FileUtils.flatten_dict(v, new_key, sep=sep).items())
+            elif isinstance(v, list):
+                for i, item in enumerate(v):
+                    if isinstance(item, dict):
+                        items.extend(
+                            FileUtils.flatten_dict(
+                                item, f"{new_key}_{i}", sep=sep
+                            ).items()
+                        )
+                    else:
+                        items.append((f"{new_key}_{i}", item))
+            else:
+                items.append((new_key, v))
+        return dict(items)
+
     @staticmethod
     def write_dict_to_csv(file_path, data):
+        flat_data = [FileUtils.flatten_dict(row) for row in data]
+        all_keys = set().union(*[row.keys() for row in flat_data])
         with open(file_path, "a", newline="", encoding="utf-8") as f:
-            writer = csv.DictWriter(f, fieldnames=data[0].keys())
+            writer = csv.DictWriter(f, fieldnames=all_keys)
             writer.writeheader()
-            for row in data:
-                writer.writerow(row)
+            for row in flat_data:
+                # Fill in missing keys with 0, replace newline characters and commas in string values
+                row_with_defaults = {
+                    key: (
+                        str(row.get(key, 0)).replace("\n", " ").replace(",", ";")
+                        if isinstance(row.get(key, 0), str)
+                        else row.get(key, 0)
+                    )
+                    for key in all_keys
+                }
+                writer.writerow(row_with_defaults)
 
     @staticmethod
     def write_json_file(file_path, data):
@@ -23,6 +56,7 @@ class FileUtils:
         with open(file_path, "r") as f:
             return json.load(f)
 
+    @staticmethod
     def check_duplicates_json_file(file_path, data):
         with open(file_path, "r") as f:
             old_data = json.load(f)
@@ -32,6 +66,7 @@ class FileUtils:
         with open(file_path, "w") as f:
             json.dump(old_data, f, indent=4)
 
+    @staticmethod
     def return_solo_items(data):
         solo_items = []
         for item in data:
